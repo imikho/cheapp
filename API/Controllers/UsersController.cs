dcdd8ce7b02f73a321adf79b6ef34265410b4960
Ivan Mikho <i.mikho@paymaxi.com>
2024-09-11 11:46:16 -0500

    End of section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+using System;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class UsersController(DataContext context): ControllerBase
+{
+    private readonly DataContext _context = context;
+
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        var users = _context.Users.ToList();
+        
+        System.Threading.Thread.Sleep(10000);
+
+        return users;
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<AppUser> GetUser(int id)
+    {
+        var user = _context.Users.Find(id);
+
+        if (user == null) return NotFound();
+
+        return user;
+    }
+}
